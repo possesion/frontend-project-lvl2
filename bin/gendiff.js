8a33a836e47ef01870fbec33af69b397e621d8d2
Pos <kazantsevgennadiy@gmail.com>
2020-06-06 13:57:19 +0300

    second step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,12 @@ import program from 'commander';
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.1.0')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action(function(filepath1, filepath2) {
+    pathValue1 = filepath1
+    pathValue2 = filepath2
+    });
 
 program.parse(process.argv);
 
