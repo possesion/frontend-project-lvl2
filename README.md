16dd8a8149e9a399e7a6b8d65f06acaf776cca23
Pos <kazantsevgennadiy@gmail.com>
2020-06-14 19:50:20 +0300

    step three

---
+++ README.md
@@ -0,0 +1,3 @@
+## Сравнение плоских файлов JSON
+ 
+<a href="https://asciinema.org/a/THnQQt5wc0dr1T8bjQZv2jafZ" target="_blank"><img src="https://asciinema.org/a/THnQQt5wc0dr1T8bjQZv2jafZ.svg" /></a>
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,17 +1,15 @@
 #!/usr/bin/env node
 import program from 'commander';
-
+import compareObjects from '../src/index.js';
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.1.0')
   .option('-f, --format [type]', 'output format')
   .arguments('<filepath1> <filepath2>')
-  .action(function(filepath1, filepath2) {
-    pathValue1 = filepath1
-    pathValue2 = filepath2
-    });
+  .action((filepath1, filepath2) => {
+    console.log(compareObjects(filepath1, filepath2));
+  });
 
 program.parse(process.argv);
-
-console.log('type \'gendiff -h\' for more info');
+//console.log('type \'gendiff -h\' for more info');
